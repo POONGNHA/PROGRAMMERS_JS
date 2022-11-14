7854437a05cb6ad0c3ea11aeea4f16f6357c3742
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-11-14 19:31:29 +0900

    두 수의 차 문제

---
+++ docs/src/lv0/두수의차.js
@@ -0,0 +1,25 @@
+// 문제 설명
+// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+// -50000 ≤ num1 ≤ 50000
+// -50000 ≤ num2 ≤ 50000
+
+// 풀이 유추
+// num1 - num2
+
+// 변수 선언 
+// num1, num2
+
+// 나의 풀이
+function solution(num1, num2) {
+    let answer = num1 - num2;
+    return answer;
+}
+
+
+// 테스트케이스
+// num1	num2	result
+// 2	3	-1
+// 100	2	98
+
+// 모범답안
+// const solution = (num1, num2) => num1 - num2
